f2a36de6bfa9341daa61b02520a28280c1cef932
Abdullah Alsabouni <21061149@stu.omu.edu.tr>
2026-04-27 13:47:49 +0300

    Routing

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,4 +1,4 @@
-const page = () => {
+const page = () => { // Next.js routes are based on the file system. When a file is added to the pages directory, it's automatically available as a route. For example, if you create a file called about.js in the pages directory, it will be available at /about.
   return (
     <div>
       <h1>About</h1>
